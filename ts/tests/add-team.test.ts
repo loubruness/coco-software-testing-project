0195dca03e73abff521d21aec0176165d143c586
Hugo PANEL <hugopanel@gmail.com>
2025-01-28 09:21:37 +0100

    Added tests

--- ts/tests/add-team.test.ts
+++ ts/tests/add-team.test.ts
@@ -1,24 +1,24 @@
-import { test, expect, chromium } from '@playwright/test'
+import { test, expect, chromium } from "@playwright/test";
 
-test('has title', async () => {
+test("has title", async () => {
   // TODO: remove 'slowMo' when done debugging
-  const browser = await chromium.launch({ slowMo: 1000 })
-  const page = await browser.newPage()
+  const browser = await chromium.launch({ slowMo: 1000 });
+  const page = await browser.newPage();
 
   // Reset database
-  await page.goto('/reset_db')
-  const proceedButton = page.locator("button:has-text('proceed')")
-  await proceedButton.click()
+  await page.goto("/reset_db");
+  const proceedButton = page.locator("button:has-text('proceed')");
+  await proceedButton.click();
 
   // Create a new team
-  await page.goto('/add_team')
-  const nameInput = page.locator('input[name="name"]')
-  const teamName = 'my team'
-  await nameInput.fill(teamName)
-  await page.click("text='Add'")
+  await page.goto("/add_team");
+  const nameInput = page.locator('input[name="name"]');
+  const teamName = "my team";
+  await nameInput.fill(teamName);
+  await page.click("text='Add'");
 
   // Check the team has been created
-  await page.goto('/teams')
-  const isVisible = await page.isVisible(`td:has-text('${teamName}')`)
-  expect(isVisible).toBe(true)
-})
+  await page.goto("/teams");
+  const isVisible = await page.isVisible(`td:has-text('${teamName}')`);
+  expect(isVisible).toBe(true);
+});
---
+++ ts/tests/duplication-of-employees.test.ts
@@ -0,0 +1,42 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("https://c.se1.hr.dmerej.info/add_employee");
+  await page.getByPlaceholder("Name").click();
+  await page.getByPlaceholder("Name").fill("test");
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill("test@test.com");
+  await page.locator("#id_address_line1").click();
+  await page.locator("#id_address_line1").fill("address");
+  await page.getByPlaceholder("City").click();
+  await page.getByPlaceholder("City").fill("city");
+  await page.getByPlaceholder("Zip code").click();
+  await page.getByPlaceholder("Zip code").fill("1234");
+  await page.getByPlaceholder("Hiring date").fill("2025-01-28");
+  await page.getByPlaceholder("Job title").click();
+  await page.getByPlaceholder("Job title").fill("job");
+  await page.getByRole("button", { name: "Add" }).click();
+
+  await page.getByRole("link", { name: "Home" }).click();
+  await page.getByRole("link", { name: "Add new employee" }).click();
+  await page.getByPlaceholder("Name").click();
+  await page.getByPlaceholder("Name").fill("test");
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill("test@test.com");
+  await page.locator("#id_address_line1").click();
+  await page.locator("#id_address_line1").fill("address");
+  await page.getByPlaceholder("City").click();
+  await page.getByPlaceholder("City").fill("city");
+  await page.getByPlaceholder("Zip code").click();
+  await page.getByPlaceholder("Zip code").fill("1234");
+  await page.getByPlaceholder("Hiring date").fill("2025-01-28");
+  await page.getByPlaceholder("Job title").click();
+  await page.getByPlaceholder("Job title").fill("job");
+  await page.getByRole("button", { name: "Add" }).click();
+
+  await page.getByRole("cell", { name: "test@test.com" }).nth(1).click();
+
+  const locator = page.getByText("test@test.com");
+  console.log(locator);
+  await expect(locator).toHaveCount(1);
+});
